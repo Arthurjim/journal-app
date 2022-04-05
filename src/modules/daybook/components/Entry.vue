e0abbbfff8b7b835e20e167eecd5a0243a649a1f
Arthurjim <arthurjim013@gmail.com>
2022-04-04 23:49:16 -0500

    Se implementan estilos para componente EntryList y Entryt

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2">
+        <div class="entry-title d-flex ">
+            <span class="text-succes fs-5 fw-bold">04</span>
+            <span class="mx-1 fs-5">Abril</span>
+            <span class="mx-5 fw-light">2022, dLunes</span>
+        </div>
+        <div class="entry-description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatibus ipsum consequuntur commodi aliquid quibusdam eaque excepturi, quidem sunt, aliquam rerum neque eligendi quod delectus doloribus exercitationem nam repudiandae omnis.
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: all .3s ease;
+    &:hover{
+        background-color: #2c3e50;
+        /* background-color: lighten($color: rgb(32, 31, 31), $amount: 45); */
+        color: #fff;
+    }
+    .entry-description{
+        font-size: 12px;
+    }
+}
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,9 +1,38 @@
 <template>
-    <h1>Entry List</h1>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Buscar entradas"
+            />
+        </div>
+        <div class="entry-scollarea">
+            <Entry v-for="item in 20" :key="item" />
+        </div>
+    </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
 export default {
-    
-}
+    components: {
+        Entry: defineAsyncComponent(()=>import('./Entry.vue')),
+    },
+
+};
 </script>
+
+<style scoped>
+input{
+    height: 40px;
+}
+.entry-list-container {
+    border-right: 1px solid #2c3e50;
+    height: calc(100vh - 50px);
+}
+.entry-scollarea{
+    height: calc(100vh - 100px);
+    overflow-y: scroll;
+}
+</style>
