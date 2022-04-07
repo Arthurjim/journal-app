5c7895c32f6a96f5d21ecf99cbe4afbc6a8bdd04
Arthurjim <arthurjim013@gmail.com>
2022-04-07 00:05:47 -0500

    Se  crea entradas ficticias

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,20 @@
 export default ()=>({
     isLoading:true,
-    entries:[1,2,3,4,5,6]
+    entries:[{
+        id:new Date().getTime(),
+        date: new Date().toDateString(),
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto omnis fugit officia incidunt temporibus dolorem distinctio itaque dolore alias explicabo nulla, totam adipisci debitis dicta quae, reprehenderit modi! Pariatur, nostrum!',
+        picture:null
+    },{
+        id:new Date().getTime()+1000,
+        date: new Date().toDateString(),
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nulla repellendus modi quas ducimus tempora, quasi suscipit amet libero, ad, quibusdam voluptates voluptas provident quod dignissimos. Ratione quidem excepturi esse?',
+        picture:null
+    },
+    {
+        id:new Date().getTime()+2000,
+        date: new Date().toDateString(),
+        text: 'Lorem 123 sit amet consectetur adipisicing elit. Iusto omnis fugit officia incidunt temporibus dolorem distinctio itaque dolore alias explicabo nulla, totam adipisci debitis dicta quae, reprehenderit modi! Pariatur, nostrum!',
+        picture:null
+    },]
 })
