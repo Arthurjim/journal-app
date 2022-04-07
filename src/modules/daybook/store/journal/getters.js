4b8c253afb53f29785d8d56675e5171bbd96bedf
Arthurjim <arthurjim013@gmail.com>
2022-04-06 23:50:59 -0500

    Se configura el state del Jorunal

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,16 @@
+// export const myGetter = (state)=>{
+    // return state
+// }
+
+
+export const getEntriesByTerm = (/*state*/)=>{
+    // return state
+}
+
+export const getEntriesById = (/*state*/)=>{
+    // return state
+}
+
+export const crreateEntry = (/*state*/)=>{
+    // return state
+}
