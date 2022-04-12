dda80b5f0acb7757dc8b195bcc0d81be6a35d349
Arthurjim <arthurjim013@gmail.com>
2022-04-12 00:23:24 -0500

    Se implementa loading para esperar la carga de las entries

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,6 +1,15 @@
 <template>
     <NavBar />
-    <div class="d-flex">
+
+    <div v-if="isLoading" class="row justify-content-md-center">
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+            <h3>
+                <i class="fa fa-spin fa-sync"></i>
+            </h3>
+        </div>  
+    </div>
+    <div v-if="!isLoading" class="d-flex">
         <div class="col-4"><EntryList/></div>
         <div class="col">
             <router-view></router-view>
@@ -10,7 +19,7 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
-import {mapActions} from "vuex";
+import {mapActions, mapState} from "vuex";
 export default {
     components: {
         NavBar: defineAsyncComponent(() => import("../components/Navbar.vue")),
@@ -20,9 +29,11 @@ export default {
     methods:{
         ...mapActions('journal',['loadEntries']),
     },
+    computed: {
+        ...mapState('journal',['isLoading']),
+    },
     created(){
         this.loadEntries();
-    
     }
 };
 </script>
