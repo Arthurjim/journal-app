da31a44f081328567bf251d89afaae99aa96f967
Arthurjim <arthurjim013@gmail.com>
2022-04-11 23:46:06 -0500

    Se instala axios y se obtiene las enties desde firebase

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -10,12 +10,19 @@
 
 <script>
 import { defineAsyncComponent } from "vue";
-
+import {mapActions} from "vuex";
 export default {
     components: {
         NavBar: defineAsyncComponent(() => import("../components/Navbar.vue")),
         EntryList: defineAsyncComponent(() => import("../components/EntryList.vue")),
 
     },
+    methods:{
+        ...mapActions('journal',['loadEntries']),
+    },
+    created(){
+        this.loadEntries();
+    
+    }
 };
 </script>
--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,15 +1,15 @@
 // export const myAction = async({commit})=>{
 
+import journalApi from "@/api/journalApi";
+
 // }
 
-export const loadEntry = async(/*{commit}*/)=>{
+export const loadEntries = async (/*{commit}*/) => {
+    console.log("loadEntries");
+    const { data } = await journalApi.get("/entries.json");
+    console.log(data);
+};
 
-}
+export const updateEntry = async (/*{commit}*/) => {};
 
-export const updateEntry = async(/*{commit}*/)=>{
-
-}
-
-export const createEntry = async(/*{commit}*/)=>{
-
-}
+export const createEntry = async (/*{commit}*/) => {};
