750774a05e4bf183a12e845bfda30634633a1983
Arthurjim <arthurjim013@gmail.com>
2022-04-14 22:55:49 -0500

    Se da formato a archivo actions

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -3,9 +3,9 @@ import journalApi from "@/api/journalApi";
 export const loadEntries = async ({ commit }) => {
     const { data } = await journalApi.get("/entries.json");
     const entries = [];
-    if(!data){
+    if (!data) {
         commit("setEntries", []);
-        return
+        return;
     }
     for (let id of Object.keys(data)) {
         entries.push({
@@ -19,27 +19,21 @@ export const loadEntries = async ({ commit }) => {
 export const updateEntry = async ({ commit }, entry) => {
     const entryData = Object.assign({}, entry);
     delete entryData.id;
-    await journalApi.put(
-        `/entries/${entry.id}.json`,
-        entryData
-    );
+    await journalApi.put(`/entries/${entry.id}.json`, entryData);
     commit("updateEntry", { ...entry });
 };
 
-export const createEntry = async ({commit},entry) => {
-   const {data} =  await journalApi.post('/entries.json',entry)
+export const createEntry = async ({ commit }, entry) => {
+    const { data } = await journalApi.post("/entries.json", entry);
     const newEntry = {
         id: data.name,
-        ...entry
-    }
-    commit("addEntry",newEntry)
-    return  data.name
+        ...entry,
+    };
+    commit("addEntry", newEntry);
+    return data.name;
 };
 
-export const deleteEntry = async({commit},id)=>{
-    await journalApi.delete(
-        `/entries/${id}.json`,
-        id
-    );
-    commit('deleteEntry',id)
-}
+export const deleteEntry = async ({ commit }, id) => {
+    await journalApi.delete(`/entries/${id}.json`, id);
+    commit("deleteEntry", id);
+};
