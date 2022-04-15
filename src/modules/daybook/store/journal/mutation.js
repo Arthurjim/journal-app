f6ae107894f6677d3d324b21cf01aa1d88a41f89
Arthurjim <arthurjim013@gmail.com>
2022-04-14 22:56:42 -0500

    Se da formato a archivo mutations

--- src/modules/daybook/store/journal/mutation.js
+++ src/modules/daybook/store/journal/mutation.js
@@ -1,18 +1,19 @@
 // export const myMutation = (state)=>{
 // }
 
-export const setEntries = (state,entries)=>{
-    state.entries = [...state.entries, ...entries]
-    state.isLoading = false
-}
+export const setEntries = (state, entries) => {
+    state.entries = [...state.entries, ...entries];
+    state.isLoading = false;
+};
 
-export const updateEntry = (state,entry)=>{
-    state.entries = state.entries.map(item =>entry.id === item.id ? entry : item)
-}
-export const addEntry = (state,entry)=>{
-    state.entries = [entry,...state.entries]
-
-}
-export const deleteEntry =(state,id)=>{
-    state.entries = state.entries.filter(entry => entry.id !== id)
-}
+export const updateEntry = (state, entry) => {
+    state.entries = state.entries.map((item) =>
+        entry.id === item.id ? entry : item
+    );
+};
+export const addEntry = (state, entry) => {
+    state.entries = [entry, ...state.entries];
+};
+export const deleteEntry = (state, id) => {
+    state.entries = state.entries.filter((entry) => entry.id !== id);
+};
