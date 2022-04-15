e4f9f63b09bf4bd6b1e115b4369486f57eb93532
Arthurjim <arthurjim013@gmail.com>
2022-04-14 22:58:29 -0500

    Se da formato a componente EntryList

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -9,51 +9,57 @@
             />
         </div>
         <div class="mt-2 d-flex flex-column">
-            <button class="btn btn-primary mx-3"
-                @click="this.$router.push({ name: 'entry', params: { id: 'new' }})"
+            <button
+                class="btn btn-primary mx-3"
+                @click="
+                    this.$router.push({ name: 'entry', params: { id: 'new' } })
+                "
             >
                 <i class="fa fa-plus-circle"></i>
                 Nueva Entrada
             </button>
         </div>
         <div class="entry-scollarea">
-            <Entry v-for="entry in entriesByTerm" :key="entry.id" :entry="entry" />
+            <Entry
+                v-for="entry in entriesByTerm"
+                :key="entry.id"
+                :entry="entry"
+            />
         </div>
     </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from 'vue';
-import { mapGetters } from 'vuex';
+import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
 
 export default {
     components: {
-        Entry: defineAsyncComponent(()=>import('./Entry.vue')),
+        Entry: defineAsyncComponent(() => import("./Entry.vue")),
     },
     computed: {
-        ...mapGetters('journal',['getEntriesByTerm']),
-       entriesByTerm() {
-          return this.getEntriesByTerm( this.term )
-        }
+        ...mapGetters("journal", ["getEntriesByTerm"]),
+        entriesByTerm() {
+            return this.getEntriesByTerm(this.term);
+        },
     },
-    data(){
-        return{
-            term: '',
-        }
-    }
-
+    data() {
+        return {
+            term: "",
+        };
+    },
 };
 </script>
 
 <style scoped>
-input{
+input {
     height: 40px;
 }
 .entry-list-container {
     border-right: 1px solid #2c3e50;
     height: calc(100vh - 50px);
 }
-.entry-scollarea{
+.entry-scollarea {
     height: calc(100vh - 100px);
     overflow-y: scroll;
 }
