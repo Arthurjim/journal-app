e02ea82eb7eabdcd32adeda5554944a968dccee8
Arthurjim <arthurjim013@gmail.com>
2022-05-24 13:06:27 -0500

    Se agrega action para login del usuario

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -4,12 +4,15 @@ const useAuth=()=>{
     const store = useStore()
     const createUser = async(user)=>{
         const resp = await store.dispatch('auth/createUser',user)
-        
         return resp
-
+    }
+    const loginUser = async(user)=>{
+        const resp = await store.dispatch('auth/loginUser',user)
+        return resp
     }
     return {
-        createUser
+        createUser,
+        loginUser
     }
 }
 export default useAuth
--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -2,20 +2,20 @@
     <span class="login100-form-title p-b-41">
         Ingresar
     </span>
-    <form class="login100-form validate-form p-b-33 p-t-5">
+    <form @submit.prevent="onSubmit" class="login100-form validate-form p-b-33 p-t-5">
 
         <div class="wrap-input100 validate-input" data-validate = "Enter username">
-            <input class="input100" type="text" placeholder="Correo" required>
+            <input v-model="userForm.email" class="input100" type="text" placeholder="Correo" required>
             <span class="focus-input100" data-placeholder="&#xe82a;"></span>
         </div>
 
         <div class="wrap-input100 validate-input" data-validate="Enter password">
-            <input class="input100" type="password" placeholder="Contraseña" autocomplete="off" required>
+            <input v-model="userForm.password" class="input100" type="password" placeholder="Contraseña" autocomplete="off" required>
             <span class="focus-input100" data-placeholder="&#xe80f;"></span>
         </div>
 
         <div class="container-login100-form-btn m-t-32">
-            <button class="login100-form-btn">
+            <button type="submit" class="login100-form-btn">
                 Login
             </button>
 
@@ -28,7 +28,27 @@
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import useAuth from '../composables/useAuth'
+import Swal from 'sweetalert2';
+import { useRouter}from 'vue-router';
 
+export default {
+    setup(){
+        const userForm = ref({
+            email: '',
+            password: ''
+        })
+        const router = useRouter()
+        const {loginUser}=useAuth()
+        return{
+            userForm,
+            onSubmit:async ()=>{
+                const {ok,message}= await loginUser(userForm.value)
+                if(!ok)Swal.fire('Error',message,'error')
+                else router.push({name:'no-entry'})
+            }
+        }
+    }
 }
 </script>
