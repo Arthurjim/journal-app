eb4480210073ed7c629a15f96fc53ff0e7c93a56
Arthurjim <arthurjim013@gmail.com>
2022-05-25 07:50:19 -0500

    Se crea interceptors de axios

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,10 @@ const journalApi = axios.create({
     baseURL:'https://vue-demos-7901a-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use((config)=>{
+    config.params={
+        auth:localStorage.getItem('idToken'),
+    }
+    return config
+})
 export default journalApi;
